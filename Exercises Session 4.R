70b5d91ceacaccaffa1a22d785f908aed2ef36c3
Jenisten <71443243+Jenisten@users.noreply.github.com>
2024-02-28 15:11:07 +0100

    Co-authored-by: Sthor20 <Sthor20@users.noreply.github.com> Co-authored-by: Leahcim00 <Leahcim00@users.noreply.github.com>

--- Exercises Session 4.R
+++ Exercises Session 4.R
@@ -1,56 +1,43 @@
 rm(list = ls())
 
-lapply(c("car", "lmtest", "sandwich", "tseries", "quantmod", "PortfolioAnalytics", "ROI.plugin.quadprog", "readxl", "zoo"), library, character.only = TRUE)
+#install.packages("vrtest")
+#install.packages("forecast")
+#install.packages("FinTS")
 
-#EXERCIES SSESION 1
+lapply(c("car", "lmtest", "sandwich", "tseries", "quantmod", "PortfolioAnalytics", "ROI.plugin.quadprog", "readxl", "zoo", "vrtest", "forecast", "FinTS"), library, character.only = TRUE)
 
-# Read excel file
-ex_1 <- read_excel("exercise 1 data.xlsx")
-print(head(ex_1))
-ts.plot(ex_1$energy)
-# Exersice 1.a
-# Should be 0 on average.
+ex_4 <- read_excel("Exercise Session 4 data.xlsx")
+ts.plot(ex_4)
+print(head(ex_4))
+# Assuming the data is log returns.
 
-# Exersice 1.b
-#Abnormal returns are thosee that does not depend on risk factors. 
+# 3.1
+Acf(ex_4, lag = 24, main = "ACF of log returns")
+Pacf(ex_4, lag = 24, main = "PACF of log returns")
+# 3.2
+Box.test(ex_4, lag = 24, type = "Ljung-Box")
+#p-value 0.02051: Can reject the null there is at least one autocorrelation different from 0
+#Variance Ratio test
+kvec <- c(2,3,5)
+Lo.Mac(ex_4,kvec)
+#                           M1        M2
+#Variance ratio = 2  1.4839378 1.2589998 - We do not reject the null here because < 1.96
+#Variance ratio = 3  1.1117969 0.9360020 - We do not reject the null here because < 1.96 
+#Variance ratio = 5  0.4686537 0.3922138 - We do not reject the null here because < 1.96 
 
-# Exersice 1.c
-# That the shccks are zero mean uncorrelated, meaning white noise.
+# 3.3
+ex_4_s <- ex_4^2
+Acf(ex_4_s, lag = 24, main = "ACF of log returns squared")
+Pacf(ex_4_s, lag = 24, main = "PACF of log returns squared")
+# 3.4
+Box.test(ex_4_s, lag = 24, type = "Ljung-Box")
+# p-value 0.7908: Can't reject the null. There is no evidence of autocorrelation.
+# Variance Ratio test
+Lo.Mac(ex_4_s, kvec)
 
-# Exersice 2
-
-# Excersice 3.1
-# Calculate excess return
-ex_1$excess_return <- ex_1$energy - ex_1$rf
-print(head(ex_1))
-
-# Calculate mean of excess return
-mean(ex_1$energy)
-mean_excess_return <- mean(ex_1$excess_return)
-print(mean_excess_return)
-
-# Exercice 3.2
-Box.test(ex_1$excess_return, lag = 26, type = "Ljung-Box")
-# we can reject the null hypothesis that the data is white noise. (p-value < 0.05).
-
-# Exercice 3.3
-
-r_it <- ex_1$energy
-f1 <- ex_1$f1
-f2 <- ex_1$f2
-f3 <- ex_1$f3
-
-
-# Contructing the linear regression models
-model <- lm (r_it ~ f1 + f2 + f3)
-
-summary(model)
-
-Ab_ret = model$coefficients[1] + model$residuals
-print(mean(Ab_ret))
-
-# Extract the intercept
-intercept <- model$coefficients[1]
-print(intercept)
-
-# Abnormal returns is the sum of the intercept and the shocks (residuals). 
+#                           M1        M2
+#Variance ratio = 2  0.1776086 0.740031 - We do not reject the null here because < 1.96
+#Variance ratio = 3  0.9130378 1.345675 - We do not reject the null here because < 1.96 
+#Variance ratio = 5  1.4281379 1.703849 - We do not reject the null here because < 1.96 
+# 3.5
+ArchTest(ex_4)
